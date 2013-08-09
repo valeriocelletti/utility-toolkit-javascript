f8a0310487601306d215748d65d2731eec69c4c3
mone <simone.fabiano@gmail.com>
2013-08-09 13:17:51 +0200

    fix code after jsdoc-oriented refactoring

--- source/Inheritance.js
+++ source/Inheritance.js
@@ -1,3 +1,18 @@
+/*
+  Copyright 2013 Weswit Srl
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 define([], function(){
   
   /**
@@ -76,8 +91,8 @@ define([], function(){
       if (!lightExtension) {
         //setup the extended class for super calls
         subClass.prototype._super_ = superClass;
-        subClass.prototype._callSuperConstructor = _callSuperConstructor;
-        subClass.prototype._callSuperMethod = _callSuperMethod;
+        subClass.prototype._callSuperConstructor = Inheritance._callSuperConstructor;
+        subClass.prototype._callSuperMethod = Inheritance._callSuperMethod;
       }
       
     },
