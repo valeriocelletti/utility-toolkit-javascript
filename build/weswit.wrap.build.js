5e15a25e9c1f027ec4f5fe330fc1294bb3572ad8
mone <simone.fabiano@gmail.com>
2013-09-04 17:54:17 +0200

    improve wrapper

--- build/weswit.wrap.build.js
+++ build/weswit.wrap.build.js
@@ -4,7 +4,7 @@
     mainConfigFile: "main.config.js",
     
     wrap: {
-        start: "(function(o) {var l='weswit/';var define = function(c,a,d){for(var b=0;b<a.length;b++)a[b]=l+a[b];o(l+c,a,d)};",
+        start: "(function(o) {var l='weswit/';var e=new RegExp('^\\./');var define = function(c,a,d){for(var b=0;b<a.length;b++)a[b]=l+a[b].replace(e,'');o(l+c,a,d)};",
         end: "}(define));"
     }
     
