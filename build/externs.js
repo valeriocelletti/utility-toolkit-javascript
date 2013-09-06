c2c36367d5bd54463f0200fd323bc048a99cc983
mone <simone.fabiano@gmail.com>
2013-09-06 18:27:01 +0200

    remove extra space

--- build/externs.js
+++ build/externs.js
@@ -1,4 +1,4 @@
 Inheritance =  function() {};
 Inheritance._super_ =  {};
 Inheritance._callSuperConstructor =  function() {};
-Inheritance._callSuperMethod =  function() {};
+Inheritance._callSuperMethod = function() {};
