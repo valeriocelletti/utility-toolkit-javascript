5e25d6aa3d4d2a561f741e0be6c6ba6a59b79973
mone <simone.fabiano@gmail.com>
2013-09-30 11:19:11 +0200

    add node externs

--- build/externs.js
+++ build/externs.js
@@ -2,3 +2,8 @@ Inheritance =  function() {};
 Inheritance._super_ =  {};
 Inheritance._callSuperConstructor =  function() {};
 Inheritance._callSuperMethod = function() {};
+
+process = {};
+process.execPath = {};
+process.v8 = {};
+process.node = {};
